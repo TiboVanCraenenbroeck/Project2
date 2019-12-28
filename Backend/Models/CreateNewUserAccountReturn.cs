ad3428389da6747e305570727412619d161e71cc
Tibovc <tibo.van.craenenbroeck@student.howest.be>
2019-12-28 17:29:37 +0100

    CreateNewUser
    - Nieuwe gebruiker aanmaken
    - SQL-connectie opgezet
    - LoginValidation verbeterd

---
+++ Backend/Models/CreateNewUserAccountReturn.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.Models
+{
+    public class CreateNewUserAccountReturn
+    {
+
+        [JsonProperty(PropertyName = "succeeded")]
+        public bool blSucceeded { get; set; }
+        
+        [JsonProperty(PropertyName = "message")]
+        public string strMessage { get; set; }
+    }
+}
