7404cd35c75c5ae85e3b0e0579748de69a3cc51e
Yente De Wael <yente.de.wael@student.howest.be>
2020-01-08 09:57:11 +0100

    API vraag + antwoord
    nog niet volledig af

---
+++ Backend/Models/Model_AnswerQuestion.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.Models
+{
+    public class Model_AnswerQuestion
+    {
+        [JsonProperty(PropertyName = "answer")]
+        public string strRightAnswer { get; set; }
+
+        [JsonProperty(PropertyName = "question")]
+        public string strQuestion { get; set; }
+
+    }
+}
