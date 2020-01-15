9b4b9eceb81f97b93b4bc0ba688c0b31fa9f71ce
Tibovc <tibo.van.craenenbroeck@student.howest.be>
2020-01-15 14:40:40 +0100

    Scherm Game - Vragen bekijken
    - Verbeteringen

---
+++ Frontend/Game/scripts/FetchDataAsync.js
@@ -0,0 +1,21 @@
+const baseURL = "https://mctproject2.azurewebsites.net/api/v1";
+
+const fetchData = async function(url, method = "GET", body = null) {
+  url = encodeURIComponent(url);
+  return fetch(`${baseURL}/${url}`, {
+    method: method,
+    body: body,
+    headers: { "content-type": "application/json" }
+  })
+    .then(r => r.json())
+    .then(data => data);
+};
+let getAPI = async function(url, callback, method = "GET", body = null) {
+  try {
+    const data = await fetchData(url, method, body);
+    console.log(data);
+    callback(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
