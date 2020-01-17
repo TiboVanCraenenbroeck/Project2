d98dd99999a9e8418140648b4919f5cbf41671e5
Désirée <desiree.verbeke@student.howest.be>
2020-01-17 16:58:39 +0100

    js leerkrachtenschermen

--- Frontend/total/scripts/questions.js
+++ Frontend/total/scripts/questions.js
@@ -1,8 +1,10 @@
 let id; 
 let decid;
+let decid2;
 let uri = "https://mctproject2.azurewebsites.net/api/v1/subject?cookie_id=";
 let domonderwerpen;
 let quiz_ids = [];
+let moeilijkheidsgraad = 0;
 
 const getDomElements = function(){
   console.log("dom geladen");
@@ -34,10 +36,12 @@ const buttonclick = function(){
     let valuesubject = subjectinput.value
     let data = {title: valuesubject, description: ''}
 
+    console.log(subjectinput.value);
     id = getCookie('id');
-    decid = id.replace("+", "%2B");
+    decid = encodeURIComponent(id);
     postdata(uri+decid, data)
     document.getElementById("js-input-onderwerp").value = "";
+
 
    /*  console.log(valuesubject); */
     /* console.log(quiz_ids); */
@@ -57,6 +61,46 @@ const buttonclick = function(){
   })
 }
 
+const moeilijkheid1 = function(){
+  let makkelijk = document.querySelector('.js-moeilijkheid1')
+  makkelijk.addEventListener('click', function(){
+    makkelijk.style.backgroundColor="red";
+    moeilijkheidsgraad = 0;
+    // terug normaal zetten andere variabelen
+    let normaal = document.querySelector('.js-moeilijkheid2');
+    let moeilijk = document.querySelector('.js-moeilijkheid3');
+    normaal.style.backgroundColor="";
+    moeilijk.style.backgroundColor="";
+
+  })
+}
+
+const moeilijkheid2 = function(){
+  let normaal = document.querySelector('.js-moeilijkheid2');
+  normaal.addEventListener('click', function(){
+    normaal.style.backgroundColor="red";
+    moeilijkheidsgraad = 1;
+    // terug normaal zetten andere variabelen
+    let makkelijk = document.querySelector('.js-moeilijkheid1');
+    let moeilijk = document.querySelector('.js-moeilijkheid3');
+    makkelijk.style.backgroundColor="";
+    moeilijk.style.backgroundColor="";
+  })
+}
+
+const moeilijkheid3 = function(){
+  let moeilijk = document.querySelector('.js-moeilijkheid3');
+  moeilijk.addEventListener('click', function(){
+    moeilijk.style.backgroundColor="red";
+    moeilijkheidsgraad = 2;
+    // terug normaal zetten andere variabelen
+    let makkelijk = document.querySelector('.js-moeilijkheid1');
+    let normaal = document.querySelector('.js-moeilijkheid2');
+    makkelijk.style.backgroundColor="";
+    normaal.style.backgroundColor="";
+  })
+}
+
 const buttonclickvragen = function(){
   let btnvragen = document.querySelector('.js-btn-verzenden');
   btnvragen.addEventListener('click', function(){
@@ -69,12 +113,12 @@ const buttonclickvragen = function(){
     var radiobtnb = document.getElementById('js-rbtn-b').checked;
     var radiobtnc = document.getElementById('js-rbtn-c').checked;
     var radiobtnd = document.getElementById('js-rbtn-d').checked; 
-    var level = document.getElementById('js-level').value;
-    var levelnr = Number(level);
+    /* var level = document.getElementById('js-level').value;
+    var levelnr = Number(level); */
 
     let data = {
       question: vraag, 
-      difficult: levelnr,
+      difficult: moeilijkheidsgraad,
       answers: [
         {
         answer: antwoorda,
@@ -95,7 +139,7 @@ const buttonclickvragen = function(){
       ]  
     }
     console.log(data);
-    var subjectinput = document.getElementById("js-input-onderwerp");
+    var subjectinput = document.getElementById("js-input-onderwerp-vragen");
     let valuesubject = subjectinput.value
     for (var quiz_id in quiz_ids)
     {
@@ -109,12 +153,12 @@ const buttonclickvragen = function(){
         localStorage.setItem('quizid', qid);
       }
     }
-
-    let onderwerpid = "bef11ca2-3fb0-4bdf-90d2-2ad0be4787e6"
+    const quizid = localStorage.getItem('quizid');
+    let onderwerpid = quizid
     let apio = "https://mctproject2.azurewebsites.net/api/v1/question/"+onderwerpid+"?cookie_id=";
    /*  let apionderwerp = "https://mctproject2.azurewebsites.net/api/v1/question/bef11ca2-3fb0-4bdf-90d2-2ad0be4787e6?cookie_id=" */
     id = getCookie('id');
-    decid = id.replace("+", "%2B");
+    decid = encodeURIComponent(id);
     
     postdata(apio+decid,data)
 
@@ -206,6 +250,9 @@ document.addEventListener('DOMContentLoaded', function()
     getDomElements();
     buttonclick();
     buttonclickvragen();
+    moeilijkheid1();
+    moeilijkheid2();
+    moeilijkheid3();
     getonderwerpen();
     getAPI("bef11ca2-3fb0-4bdf-90d2-2ad0be4787e6");
 });
