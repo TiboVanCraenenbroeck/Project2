ae71ed44524b1f2baef2f0a5a3d32e715d90bf17
aiko <aiko.van.ryssel@student.howest.be>
2020-01-19 14:07:22 +0100

    samenvoegen schermen avatar en loading
    data van scherm avatar naar loading sturen

--- Frontend/scripts/avatar.js
+++ Frontend/scripts/avatar.js
@@ -1,76 +1,97 @@
-let teamAName,teamBName,couter=0,teamAAvatarId="69fbbdd6-bfbb-4802-8b0d-3e37350ced3d",teamBAvatarId="69fbbdd6-bfbb-4802-8b0d-3e37350ced3c";
-let dataName;
-let teamNamechange; 
-
-
-
-
-const raketKeuze=()=>{
-    let raket= document.querySelector(".js-raket");
-    raket.addEventListener('click', function() {
-        console.log(raket.value)
-
+// Vars
+let domDivRockets, domRockets, domNext, domInputTeamName, domTitleTeam,teamRocket;
+const letters = ["A", "B"];
+let teamAvatars = [],
+  teamNames = [],
+  team = 0,
+  teamNowAvatarId,
+  maxTeams = 2;
+const checkIfMaxTeamsIsReady = function() {
+  if (team == maxTeams) {
+    // Send the teams to the loadingsScreen
+    window.location.href = "loadscreen.html";
+  }
+};
+const next = function() {
+  // Check if the team has selected an avatar
+  if (teamNowAvatarId != null) {
+    // Check if the team has choosen an name
+    const teamName = domInputTeamName.value;
+    if (teamName != "") {
+      // Save it in the local storage
+      localStorage.setItem(`teamAvatar${letters[team]}`, teamNowAvatarId);
+      localStorage.setItem(`teamnaam${letters[team]}`, teamName);
+      localStorage.setItem(`teamAvatarName${letters[team]}`,teamRocket)
+      // Reset all the fields
+      teamNowAvatarId = null;
+      domInputTeamName.value = "";
+      team++;
+      domTitleTeam.innerHTML = `Teamnaam groep ${team + 1}`;
+      // Check if all teams have an avatar
+      checkIfMaxTeamsIsReady();
+    } else {
+      alert("Je moet een teamnaam invullen");
+    }
+  } else {
+    alert("Je moet een raket kiezen");
+  }
+};
+const clearSelectedRocket = function() {
+  for (const domRocket of domRockets) {
+    // Check if the team already has selected an avatar
+    if (domRocket.getAttribute("data-avatarSelected") == team + 1) {
+      domRocket.setAttribute("data-avatarSelected", 0);
+    }
+  }
+};
+const rocketSelected = function(domRocket) {
+  // Check if the element already is taken
+  if (domRocket.getAttribute("data-avatarSelected") == 0) {
+    // Check if the team has already selected an avatar
+    if (teamNowAvatarId != null) {
+      clearSelectedRocket();
+    }
+    // Get the avatarId
+    teamNowAvatarId = domRocket.getAttribute("data-avatarId");
+    teamRocket=domRocket.getAttribute("name")
+    domRocket.setAttribute("data-avatarSelected", team + 1);
+  }
+};
+const rocketsAddEventlistener = function() {
+  domRockets = document.querySelectorAll(".js-rockets");
+  for (const domRocket of domRockets) {
+    domRocket.addEventListener("click", function() {
+      rocketSelected(domRocket);
     });
-
+  }
+};
+const loadAvatars = function(data) {
+  // Set all the rockets into the DOM
+  let output = "";
+  for (const rocket of data) {
+    output += `<div class="gallery__item gallery__item js-rockets" data-avatarId="${rocket["avatar_id"]}" name="${rocket["name"]}" data-avatarSelected="0">
+      <img src="https://aikovanryssel.github.io/project2IMG/img/raket/svg/${rocket["name"]}.svg"
+              class="gallery__img" value="raketdonkerroodroze.svg" alt="Avatar ${rocket["name"]}">
+</div>`;  
 }
-const teamA =()=>{
-    let volgende= document.querySelector(".js-volgende")
-    let naamverandering = document.querySelector(".js-name");
-    volgende.addEventListener('click', function() {
-    let teamNaam=document.getElementById('username').value
-        console.log("klik");
-        //console.log(teamNaam)
-
-        if(couter==1)
-        {
-            teamBName=teamNaam;
-            console.log(teamAName,teamBName);
-            if (typeof(Storage) !== "undefined") {
-                // Store
-                localStorage.setItem('teamnaamA',teamAName); 
-                localStorage.setItem('teamnaamB',teamBName); 
-                localStorage.setItem('teamAvatarA',teamAAvatarId); 
-                localStorage.setItem('teamAvatarB',teamBAvatarId); 
-
-
-                let teamnaamA,teamnaamB,teamAvatarA,teamAvatarB;
-                teamnaamA=localStorage.getItem('teamnaamA');
-                teamnaamB=localStorage.getItem('teamnaamB');
-                teamAvatarA=localStorage.getItem('teamAvatarA');
-                teamAvatarB=localStorage.getItem('teamAvatarB');
-                //console.log("teama: "+localStorage.getItem('teamnaamA')+" teamb: "+localStorage.getItem('teamnaamB')+" avatara: "+localStorage.getItem('teamAvatarA')+" avatarb: "+localStorage.getItem('teamAvatarB'))
-                // Retrieve
-              }
-            teamAName="";
-            teamBName="";
-            couter=0;
-         
-        }
-        else
-        {
-            let teamBHTML='';
-            let teamNamechange = '';
-            teamNamechange = `<h4>Team naam groep 2</h4>`
-            teamBHTML=`<a class="js-volgende" href="./loadscreen.html">Start Spel</a>`;
-            volgende.innerHTML = teamBHTML;
-            naamverandering.innerHTML = teamNamechange;
-            teamAName=teamNaam;
-            couter=1;
-          
-        }
-    });
-}
-
-
-
-
- 
-
-//DOM
-document.addEventListener('DOMContentLoaded', function() 
-{
-	console.info('DOM is ready to roll.. 👌');
-    teamA();
-    raketKeuze();
-
+  domDivRockets.innerHTML = output;
+  // Set an eventlistener on the rockets
+  rocketsAddEventlistener();
+};
+const loadDOM = function() {
+  domDivRockets = document.querySelector(".js-div--rockets");
+  domNext = document.querySelector(".js-volgende");
+  domInputTeamName = document.querySelector(".js-teamname");
+  domTitleTeam = document.querySelector(".js-titleTeam");
+  // Check if someone clicked on the btn
+  domNext.addEventListener("click", function() {
+    next();
+  });
+};
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Geladen😎");
+  // Load the dom-elements
+  loadDOM();
+  // Load the avatars
+  getAPI("avatars", loadAvatars);
 });
---
+++ Frontend/scripts/index.js
@@ -0,0 +1,22 @@
+let domBtnStartSpel,domBtnLeerkracht;
+const btnStartSpel=()=>{
+    domBtnStartSpel.addEventListener("click", function() {
+        window.location.href = "avatar.html";
+      });
+}
+const btnLeerkrachten=()=>{
+    domBtnLeerkracht.addEventListener("click", function() {
+        window.location.href = "total/index.html";
+
+      });
+}
+const init=()=>{
+    domBtnStartSpel=document.querySelector('.js-btnStart');
+    domBtnLeerkracht=document.querySelector('.js-btnLeerkracht')
+}
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Geladen😎");
+    init();
+    btnStartSpel();
+    btnLeerkrachten();
+  });
